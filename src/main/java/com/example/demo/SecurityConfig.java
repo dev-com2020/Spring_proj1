3e36e8678a72d6eb49d72b50ef42489bdaa4e39f
dev-com2020 <kwp@tlen.pl>
2023-04-27 15:58:44 +0200

    final 16:00

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -15,6 +18,23 @@ public class SecurityConfig {
         return username -> repo.findByUsername(username).asUser();
 
     }
+
+    @Bean
+    SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/","/search").authenticated()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET,"/api/**").authenticated()
+                .requestMatchers(HttpMethod.POST,"/new-video","/api/**").hasRole("ADMIN")
+                .anyRequest().denyAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
     @Bean
     CommandLineRunner initUsers(UserManagementRepository repository){
         return args -> {
